871a53ade197463bdc0bbb14076bb108cfacfbe9
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-06 21:11:29 +0100

    Update repo.py

--- ansible_unifi_network/library/repo.py
+++ ansible_unifi_network/library/repo.py
@@ -3,13 +3,27 @@
 from ansible.module_utils.basic import *
 
 
+def github_repo_present(data):
+    has_changed = False
+    meta = {"present": "not yet implemented"}
+    return (has_changed, meta)
+
+def repo_absent(data=None):
+    has_changed = False
+    meta = {"absent": "not yet implemented"}
+
 def main():
     fields = {
         "name": {"type": "str", "default": "test"}
     }
+    choice_map = {
+        "present": repo_present,
+        "absent": repo_absent
+    }
 
     module = AnsibleModule(argument_spec=fields)
-    module.exit_json(changed=False, meta=module.params)
+    has_changed, result = choice_map.get(module.params['state'])(module.params)
+    module.exit_json(changed=False, meta=result)
 
 
 if __name__ == "__main__":
