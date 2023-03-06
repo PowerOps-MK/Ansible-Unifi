87db55ba9f0a05b0c1741fbf06d1361055cfc82d
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-06 20:56:34 +0100

    Update repo.py

--- ansible_unifi_network/library/repo.py
+++ ansible_unifi_network/library/repo.py
@@ -2,12 +2,11 @@
 
 from ansible.module_utils.basic import *
 
+
 def main():
+    module = AnsibleModule(argument_spec={})
+    response = {"hello": "world"}
+    module.exit_json(changed=False, meta=response)
 
-	module = AnsibleModule(argument_spec={})
-	response = {"hello": "world"}
-	module.exit_json(changed=False, meta=response)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
