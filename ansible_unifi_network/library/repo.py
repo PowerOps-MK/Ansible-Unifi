76d75f49da2e99553e775a87d38b4c4353f4e4d3
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-06 21:03:31 +0100

    Update repo.py

--- ansible_unifi_network/library/repo.py
+++ ansible_unifi_network/library/repo.py
@@ -4,9 +4,13 @@ from ansible.module_utils.basic import *
 
 
 def main():
-    module = AnsibleModule(argument_spec={})
-    response = {"hello": "world"}
-    module.exit_json(changed=False, meta=response)
+    field = {
+        "name": {"required": True, "type": "str", "default": "test"}
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+    module.exit_json(changed=False, meta=module.params)
+
 
 if __name__ == "__main__":
     main()
