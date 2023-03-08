995d832b935023c0b11522f5411d1c21ef4c95aa
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-08 18:21:26 +0100

    Update unifi.py

--- ansible_unifi_network/library/unifi.py
+++ ansible_unifi_network/library/unifi.py
@@ -64,18 +64,18 @@ message:
 
 
 def group_present(module):
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
-    if module.check_mode:
-        module.exit_json(**result)
-
     # seed the result dict in the object
     # we primarily care about changed and state
     # changed is if this module effectively modified the target
     # state will include any data that you want your module to pass back
     # for consumption, for example, in a subsequent task
     result = dict(changed=False, original_message="", message="")
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
 
     # manipulate or modify the state as needed (this is going to be the
     # part where your module will do what it needs to do)
@@ -97,13 +97,16 @@ def group_present(module):
     # simple AnsibleModule.exit_json(), passing the key/value results
     module.exit_json(**result)
 
+
 def group_absent(module):
     result = dict(changed=False, message="")
     result["message"] = "absent mode"
-    module.exit_json(**result)
+
+    return True, False, result
+
 
 def main():
-    # define available arguments/parameters a user can pass to the module
+    # define parameters for the module
     module_args = dict(
         name=dict(type="str", required=True),
         state=dict(type="str", default="present", choices=["present", "absent"]),
@@ -113,12 +116,10 @@ def main():
       "absent": group_absent
     }
 
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
+    # AnsibleModule object for abstraction of Ansible
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
-    choice_map.get(module.params["state"])(module)
+    has_changed, result = choice_map.get(module.params["state"])(module)
+    module.exit_json(changed=has_changed, meta=result)
 
 if __name__ == "__main__":
     main()
