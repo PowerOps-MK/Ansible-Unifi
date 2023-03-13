d2880318e2a10051556afc469313fe90665b6fa6
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-13 07:52:12 +0100

    Update t.py

--- plugins/modules/t.py
+++ plugins/modules/t.py
@@ -4,6 +4,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from ansible.module_utils.basic import AnsibleModule
+from __future__ import (absolute_import, division, print_function)
 
 DOCUMENTATION = r"""
 ---
