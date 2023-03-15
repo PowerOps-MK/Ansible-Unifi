7b00da045c741a871f942ebf60d232fda5057fa2
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-15 21:03:46 +0100

    Update port_group.py

--- plugins/modules/port_group.py
+++ plugins/modules/port_group.py
@@ -83,7 +83,8 @@ from ansible.module_utils.basic import AnsibleModule
 # Parameters
 site = "default"
 resource = "firewallgroup/"
-url = f"https://localhost:8443/api/s/{site}/rest/{resource}"
+login_url = "https://localhost:8443/api/login"
+api_url = f"https://localhost:8443/api/s/{site}/rest/{resource}"
 username = "unifi"
 password = "6VK8eK92ePP*dHR6"
 
