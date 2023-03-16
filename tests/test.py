0e159d6fd9a58787099ec736dc9bb84f5719c637
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-16 22:00:46 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -9,6 +9,6 @@ username = "unifi"
 password = "6VK8eK92ePP*dHR6"
 
 response = open_url(url='https://localhost:8443/status', method="GET", validate_certs=False, headers={'Content-Type':'application/json'})
-print(response)
+print(response.content)
 
 # force_basic_auth=True, url_username=username, url_password=password, 
