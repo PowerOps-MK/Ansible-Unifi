29cf81b4cc276568e0468ba4341443cf1c763278
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-16 19:50:23 +0100

    Update port_group.py

--- plugins/modules/port_group.py
+++ plugins/modules/port_group.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # (c) 2012, Elliott Foster <elliott@fourkitchens.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
