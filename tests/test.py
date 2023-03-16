54344db402c0736f28214757da4c31f4d7d7c2d9
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-16 21:39:17 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,2 +1,13 @@
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import *
+
+site = "default"
+resource = "firewallgroup/"
+login_url = "https://localhost:8443/api/login"
+api_url = f"https://localhost:8443/api/s/{site}/rest/{resource}"
+username = "unifi"
+password = "6VK8eK92ePP*dHR6"
+
+
+response = open_url(url=login_url, method="POST", validate_certs=False, force_basic_auth=True, url_username=username, url_password=password)
+print(response)
