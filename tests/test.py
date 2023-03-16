23964a1ec9759019b21032cd43ddad9dfd90cf13
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-16 22:16:05 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ api_url = f"https://localhost:8443/api/s/{site}/rest/{resource}"
 username = "unifi"
 password = "6VK8eK92ePP*dHR6"
 
-response = open_url(url='https://localhost:8443/api/login/', method="POST", validate_certs=False, headers={'Content-Type':'application/json'})
+response = open_url(url=login_url, method="POST", validate_certs=False)
 print(response.read())
 
 # force_basic_auth=True, url_username=username, url_password=password, 
