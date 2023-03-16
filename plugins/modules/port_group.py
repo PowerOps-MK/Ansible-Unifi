963d64c595f1f9a321b840ef0c3d593908588bcb
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-16 20:40:42 +0100

    Update port_group.py

--- plugins/modules/port_group.py
+++ plugins/modules/port_group.py
@@ -93,8 +93,10 @@ def present(module):
     try:
         # Create result dict
         result = dict(result=module.params["members"])
-
-        response = open_url(login_url, method="POST", validate_certs=False, url_username=username, url_password=password)
+        
+        print(login_url)
+        response = open_url(login_url, method="POST", validate_certs=False, force_basic_auth=True, url_username=username, url_password=password)
+        
         return True, result
     except BaseException as e:
         module.fail_json(msg=e)
