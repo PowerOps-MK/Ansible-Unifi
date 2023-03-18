a72d80b275656f0f8cdb2e2dce45c4db4427394e
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-18 20:36:10 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -8,4 +8,4 @@ api_url = f"https://localhost:8443/api/s/{site}/rest/{resource}"
 username = "unifi"
 password = "6VK8eK92ePP*dHR6"
 
-open_url(url="https://localhost:8443/api/s/default/rest/firewallrule", method="GET", validate_certs=False, force_basic_auth=True, url_username=username, url_password=password)
+open_url(url="https://localhost:8443/api/s/default/rest/firewallrule", method="GET", validate_certs=False, data={"username": username, "password": password})
