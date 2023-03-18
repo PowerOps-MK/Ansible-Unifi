4763e28a6974080e8917f8bd8294096975a881c3
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-18 20:25:08 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -9,7 +9,4 @@ api_url = f"https://localhost:8443/api/s/{site}/rest/{resource}"
 username = "unifi"
 password = "6VK8eK92ePP*dHR6"
 
-# response = open_url(url="https://localhost:8443/api/s/default/rest/firewallrule", method="GET", validate_certs=False, force_basic_auth=True, url_username=username, url_password=password)
-
-response = urlopen("https://www.example.com")
-
+open_url(url="https://localhost:8443/api/s/default/rest/firewallrule", method="GET", validate_certs=False, force_basic_auth=True, url_username=username, url_password=password)
