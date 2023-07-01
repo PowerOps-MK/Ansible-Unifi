fbb86a77eac5b430a20acf94300960fcc985fa2e
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-07-01 23:03:57 +0200

    Update create_session.py

--- plugins/modules/create_session.py
+++ plugins/modules/create_session.py
@@ -53,9 +53,8 @@ def authenticate(module):
 
         session = Request()  # pylint: disable=E0602
         session.post(url=login_url, validate_certs=False, data=module.jsonify(payload))
-        session_dict = session.__dict__
-        session_json = module.jsonify(session_dict)
-        return session_json
+
+        return session
 
     except BaseException:
         module.fail_json(msg="Authenication to API had failed")
