0eec3f54b40ef251db56599e0d8d62737722a7b7
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-07-03 22:40:12 +0200

    Update create_session.py

--- plugins/module_utils/create_session.py
+++ plugins/module_utils/create_session.py
@@ -7,33 +7,6 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-
-DOCUMENTATION = """
----
-module: create_session
-short_description: Create a session object.
-description:
-    - Creates a session object with authentication to a Unifi Network via the REST API.
-author:
-  - Mr PotatoHead (@mrpotatohead)
-version_added: "1.0.0"
-"""
-
-EXAMPLES = r"""
-- name: Run the custom module
-  unifi.network.create_session:
-
-- name: Run the custom module
-  unifi.network.create_session:
-"""
-
-RETURN = r"""
-changed:
-    description: boolean if a resource is changed.
-    type: bool
-    returned: always
-"""
-
 # Modules
 # from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import Request
